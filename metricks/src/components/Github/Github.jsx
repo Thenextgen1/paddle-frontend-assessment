8f3eece2dd51bb4482c3b32ac100accc58b3391c
Elijah Ohiwerei <elijahohimairei@gmail.com>
2022-06-03 22:07:30 +0100

    added new dependencies and built github component

--- metricks/src/components/Github/Github.jsx
+++ metricks/src/components/Github/Github.jsx
@@ -1,16 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import InfiniteScroll from "react-infinite-scroll-component";
+import millify from "millify";
+import moment from "moment";
 import '../../styles/github.css'
 
 
 const Github = () => {
 
     const [repo, setRepo] = useState([]);
-    const page = 4
-    useEffect(() => {
-        fetch(`https://api.github.com/search/repositories?q=created:>2022-05-02&sort=stars&order=desc&page=${page}`)
+    const [page, setPage] = useState(1)
+
+    const fetchData = (setItems, items) => {
+        fetch(`https://api.github.com/search/repositories?q=created:>2022-05-03&sort=stars&order=desc&page=${page}`)
             .then(res => res.json())
-            .then((data) => setRepo(data.items))
+            .then((data) => setItems([...items, ...data.items]))
+        setPage(prevPage => prevPage + 1)
+    }
+
+    useEffect(() => {
+        fetchData(setRepo, repo);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+
+
+
 
 
     console.log(repo)
@@ -21,10 +35,14 @@ const Github = () => {
             <div className='repo-card-content'>
                 <h2>{data.name}</h2>
                 <p>{data.description}</p>
-                <div>
-                    <p>{data.stargazers_count}</p>
-                    <p>{data.open_issues}</p>
-                    <p></p>
+                <div className='statistics'>
+                    <div>
+                        <p className='box'><a href={data.html_url} alt="repo" target="_blank" rel="noreferrer">{millify(data.stargazers_count, {
+                            precision: 2
+                        })} Stars</a></p>
+                        <p className='box'><a href={data.html_url} alt="repo" target="_blank" rel="noreferrer">{data.open_issues} Issues</a></p>
+                    </div>
+                    <p>{moment(data.created_at).startOf('ss').fromNow()}</p>
                 </div>
             </div>
         </section>
@@ -33,9 +51,20 @@ const Github = () => {
 
 
     return (
-        <div>
-            {allRepo}
-        </div>
+        <main className='github-main'>
+            <h1>Most Starred Repos from the last 30 days</h1>
+            <InfiniteScroll
+                dataLength={repo.length}
+                next={() => {
+                    fetchData(setRepo, repo)
+                }}
+                hasMore={true}
+                loader={<h4>Loading...</h4>}
+            >
+                {allRepo}
+            </InfiniteScroll>
+
+        </main>
 
     )
 }
