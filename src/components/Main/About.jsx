efb8c31280589ec509d875d1e85b4d3b8a4aede9
Elijah Ohiwerei <elijahohimairei@gmail.com>
2022-06-09 23:39:17 +0100

    Created html tag structure

--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Navbar, Footer } from '../../components'
+import { Navbar, Footer } from '../../components';
+import { Link } from 'react-router-dom';
 import '../../styles/about.css'
 
 
@@ -8,7 +9,40 @@ const About = () => {
 
     <>
       <Navbar />
-      <main className='about'></main>
+      <main className='about'>
+        <section>
+          <div>
+            <h4><span></span>ABOUT US</h4>
+            <h3>Built for SaaS and E-commerce</h3>
+          </div>
+          <div>
+            <p>Our tools are easy to set up and use with a user friendly dashboard that enables you to set up, launch, automate and manage multi-affiliate campaigns in 5 minutes.</p>
+          </div>
+        </section>
+
+        <section>
+          <h3>Metricks was developed because just like you, we needed a product that could give us good value.</h3>
+          <div>
+            <div>
+              <h5><span>01</span> WHY US?</h5>
+              <p>We pride ourselves in our ability to innovate and create world-class tools that provide reliable and efficient touchpoints between advertisers and affiliates.</p>
+            </div>
+            <div>
+              <h5><span>02</span>GROWING WITH YOU</h5>
+              <p>Leveraging the best technology, we have developed an all-in-one affiliate marketing tracking software, a genius tool to help you track, automate and optimize your influencer campaigns, all from one dashboard.</p>
+              <p>Our team of experts are constantly brainstorming, testing and developing new solutions with only one thing in mind - your business growth. Your success is our success and by giving you the tools you need to scale, we grow as well.</p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <h5>Got a Question</h5>
+          <p>See how Metricks can help your business grow with best Affiliate Marketing Tracking Software.</p>
+          <Link to="/">Contact us</Link>
+        </section>
+
+
+      </main>
       <Footer />
     </>
   )
--- src/components/Main/Blog.jsx
+++ src/components/Main/Blog.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import { Navbar, Footer } from '../../components';
+import { dataSet } from '../../services/data';
 import '../../styles/blog.css';
 
 
 const Blog = () => {
+
+    const blogData = dataSet.map((data) => (
+        <div id={data.id}>
+            <img src={data.image} alt="post" />
+            <h5>{data.datePosted}</h5>
+            <h4>{data.heading}</h4>
+            <p>{data.content}</p>
+            <p>READ MORE</p>
+        </div>
+    ))
+
+
+
     return (
         <>
             <Navbar />
-            <div>Blog</div>
+            <main>
+                <section>
+                    <h3>BLOG</h3>
+                    <h3>Articles and News</h3>
+
+                </section>
+                <section>
+                    <div>
+                        <h4>Latest From The Blog</h4>
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                        <div>
+                            <input type="search" placeholder="Search here" />
+                        </div>
+                    </div>
+
+                    {blogData}
+                </section>
+
+
+
+            </main>
             <Footer />
         </>
     )
