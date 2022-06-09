dc46dedeaabd08b4e79424957c77f2f80df5d959
Elijah Ohiwerei <elijahohimairei@gmail.com>
2022-06-09 23:40:38 +0100

    Created JSON data

---
+++ src/services/data.js
@@ -0,0 +1,50 @@
+export const dataSet = [
+    {
+        id: 1,
+        image: '',
+        datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
+        heading: '12 Popup Use Cases To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+
+    },
+    {
+        id: 2,
+        image: '',
+        datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
+        heading: '12 Popup Use Cases To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+
+    },
+    {
+        id: 3,
+        image: '',
+        datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
+        heading: '12 Popup Use Cases To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+
+    },
+    {
+        id: 4,
+        image: '',
+        datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
+        heading: '12 Popup Use Cases To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+
+    },
+    {
+        id: 5,
+        image: '',
+        datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
+        heading: '12 Popup Use Cases To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+
+    },
+    {
+        id: 6,
+        image: '',
+        datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
+        heading: '12 Popup Use Cases To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+
+    }
+]
