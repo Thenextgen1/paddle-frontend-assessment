609530507ca1c15c9fd0e53f10150197f8783f9b
Elijah Ohiwerei <elijahohimairei@gmail.com>
2022-06-13 22:16:53 +0100

    formatted data.js page

--- src/services/data.js
+++ src/services/data.js
@@ -1,50 +1,82 @@
+import blogImg from '../assets/blog-pic.jpeg'
+
+
 export const dataSet = [
     {
         id: 1,
-        image: '',
+        image: blogImg,
         datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
-        heading: '12 Popup Use Cases To Increase Conversions',
-        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+        heading: '12 Popup Use Cases ',
+        subHeading: 'To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo',
 
     },
     {
         id: 2,
-        image: '',
+        image: blogImg,
         datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
-        heading: '12 Popup Use Cases To Increase Conversions',
-        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+        heading: '12 Popup Use Cases ',
+        subHeading: 'To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo',
 
     },
     {
         id: 3,
-        image: '',
+        image: blogImg,
         datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
-        heading: '12 Popup Use Cases To Increase Conversions',
-        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+        heading: '12 Popup Use Cases ',
+        subHeading: 'To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo',
 
     },
     {
         id: 4,
-        image: '',
+        image: blogImg,
         datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
-        heading: '12 Popup Use Cases To Increase Conversions',
-        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+        heading: '12 Popup Use Cases ',
+        subHeading: 'To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo',
 
     },
     {
         id: 5,
-        image: '',
+        image: blogImg,
         datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
-        heading: '12 Popup Use Cases To Increase Conversions',
-        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+        heading: '12 Popup Use Cases ',
+        subHeading: 'To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo',
 
     },
     {
         id: 6,
-        image: '',
+        image: blogImg,
         datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE . AFFILATE',
-        heading: '12 Popup Use Cases To Increase Conversions',
-        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+        heading: '12 Popup Use Cases ',
+        subHeading: 'To Increase Conversions',
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo',
 
     }
+]
+
+
+export const articleset = [
+    {
+        id: 1,
+        datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE',
+        heading: '12 Popup Use Cases To Increase Conversions',
+        content: '2. Unless specified otherwise, all Data requested by Metricks is mandatory and failure to provide this Data may make it impossible for Metricks to'
+    },
+    {
+        id: 2,
+        datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE',
+        heading: '12 Popup Use Cases To Increase Conversions',
+        content: '2. Unless specified otherwise, all Data requested by Metricks is mandatory and failure to provide this Data may make it impossible for Metricks to'
+    },
+    {
+        id: 3,
+        datePosted: 'SEPTEMBER 6, 2021 . FEATURED ARTICLE',
+        heading: '12 Popup Use Cases To Increase Conversions',
+        content: '2. Unless specified otherwise, all Data requested by Metricks is mandatory and failure to provide this Data may make it impossible for Metricks to'
+    },
+
 ]
